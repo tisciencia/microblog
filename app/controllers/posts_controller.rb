730bfdffe0c48466e443cd6ab3b5eecd5a0717fb
Tiago Sciencia <tisciencia@gmail.com>
2015-01-26 20:37:37 -0200

    update layout and create CRUP for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to posts_path, :flash => { :success => "Post updated." }
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path, :flash => { :success => "Post created." }
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    Post.find(params[:id]).destroy
+    redirect_to posts_path, :flash => { :success => "Post deleted." }
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :slug, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'home#index'
 
   get 'contact' => 'home#contact', as: :contact
   get 'about-me' => 'home#aboutme', as: :aboutme
+
+  resources :posts
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
