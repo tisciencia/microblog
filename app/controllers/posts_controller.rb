341159490fe5a2b84bf80772ecae3ce7d3f39c86
Tiago Sciencia <tisciencia@gmail.com>
2015-01-29 21:17:25 -0200

    updated slug, aboutme and readme

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,15 +8,15 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find_by_slug(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
+    @post = Post.find_by_slug(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
+    @post = Post.find_by_slug(params[:id])
     if @post.update_attributes(post_params)
       redirect_to posts_path, :flash => { :success => "Post updated." }
     else
@@ -34,13 +34,13 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    Post.find(params[:id]).destroy
+    Post.find_by_slug(params[:id]).destroy
     redirect_to posts_path, :flash => { :success => "Post deleted." }
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :slug, :content)
+    params.require(:post).permit(:title, :content)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,16 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :slug, presence: true
   validates :content, presence: true
+
+  before_save :correct_slug!
+
+  def to_param
+    slug.parameterize
+  end
+
+  private
+
+  def correct_slug!
+    self.slug = title.parameterize
+  end
 end
